ee77f6d766aeb0327b4e79fdd327e4f77027f256
bitch <169540526+domeDiff@users.noreply.github.com>
2026-08-12 23:00:30 +0530

    complete

--- Assets/CreativeCore_Animation/Scripts/DoorTrigger.cs
+++ Assets/CreativeCore_Animation/Scripts/DoorTrigger.cs
@@ -8,11 +8,11 @@ public class DoorTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        anim.SetTrigger("PlayerProximity");
+        anim.SetTrigger("playerProximity");
     }
 
     private void OnTriggerExit(Collider other)
     {
-        anim.SetTrigger("PlayerProximity");
+        anim.SetTrigger("playerProximity");
     }
 }
